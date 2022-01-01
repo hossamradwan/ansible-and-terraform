00e0e61c680befb9c5587d16f732f790b3faf7b9
hossamradwan <hossam.radwan.engineer@gmail.com>
2022-01-01 22:16:51 +0200

    ansible with terraform

---
+++ main.tf
@@ -0,0 +1,155 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+variable "vpc_cidr_block" {}
+
+variable "subnet_cidr_block" {}
+
+variable "avail_zone" {}
+
+variable "env_prefix" {}
+
+variable "my_ip" {}
+
+variable instance_type {}
+variable public_key_location {}
+
+variable "ssh_key_private" {}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
+  enable_dns_hostnames = true
+  tags = {
+    Name: "${var.env_prefix}-vpc"
+
+  }
+}
+
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.subnet_cidr_block
+  availability_zone = var.avail_zone
+  tags = {
+    "Name" = "${var.env_prefix}-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    "Name" = "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route  {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  
+  tags = {
+    "Name" = "${var.env_prefix}-main-rtb"
+  }
+
+}
+
+resource "aws_default_security_group" "default-sg" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    #opens a range of ports
+    from_port = 22
+    to_port = 22
+    ######################
+    protocol = "tcp"
+    cidr_blocks = [var.my_ip] #who is allowed to access
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    # any port
+    from_port = 0
+    to_port = 0
+    protocol = "-1"  # any protocol
+    cidr_blocks = ["0.0.0.0/0"] 
+    prefix_list_ids = [] #allow access to vpc endpoint
+  }
+
+  tags = {
+    "Name" = "${var.env_prefix}-default-sg"
+  }
+
+}
+
+data "aws_ami" "latest-amazon-linux-image" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+  #example of another filter
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "ssh-key" {
+  key_name = "terraform-server-key-pair-2"
+  public_key = file(var.public_key_location)
+}
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id #ec2 image id
+  #ami = "ami-0fb653ca2d3203ac1"  # id of ubuntu image
+  instance_type = var.instance_type
+
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [ aws_default_security_group.default-sg.id ]
+  availability_zone = var.avail_zone
+
+  #allow external traffic
+  associate_public_ip_address = true
+
+  key_name = aws_key_pair.ssh-key.key_name
+
+  tags = {
+    "Name" : "${var.env_prefix}"
+  }
+
+  # provisioner "local-exec" {
+  #   working_dir = "./ansible/"
+  #   # --inventory overrides the host file
+  #   command = "ansible-playbook --inventory ${self.public_ip}, --private-key ${var.ssh_key_private} --user ec2-user deploy-docker-new-user.yaml" 
+
+  # }
+
+}
+
+resource "null_resource" "configure_server" {
+  triggers = {
+    trigger = aws_instance.myapp-server.public_ip
+  }
+
+  provisioner "local-exec" {
+    working_dir = "./ansible/"
+    # --inventory overrides the host file
+    command = "ansible-playbook --inventory ${aws_instance.myapp-server.public_ip}, --private-key ${var.ssh_key_private} --user ec2-user deploy-docker-new-user.yaml" 
+  }
+}
+
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
+}
+
+
